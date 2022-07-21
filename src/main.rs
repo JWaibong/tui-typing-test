4296f5bd4a08ad725b539f979c474671b7e8e7a3
Jonathan Ng <jonathan.ng@stonybrook.edu>
2022-07-21 03:01:34 -0400

    Game page has a working timer / countdown bar and users can input text

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use tui::{
     style::{Color, Modifier, Style},
     text::{Span, Spans},
     widgets::{
-        Block, BorderType, Borders, Cell, List, ListItem, ListState, Paragraph, Row, Table, Tabs,
+        Block, BorderType, Borders, Cell, List, ListItem, ListState, Paragraph, Row, Table, Tabs, Gauge,
     },
     Terminal,
 };
@@ -39,6 +39,7 @@ enum Event<I> {
 enum MenuItem {
     Home,
     Game,
+    GameOver,
 }
 
 impl From<MenuItem> for usize {
@@ -46,6 +47,7 @@ impl From<MenuItem> for usize {
         match input {
             MenuItem::Home => 0,
             MenuItem::Game => 1,
+            MenuItem::GameOver => 2,
         }
     }
 }
@@ -83,9 +85,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     terminal.clear()?;
 
     let menu_titles = vec!["Start", "Quit"];
+    let mut curr_input = String::from("");
     let mut start = false; 
-    let mut i : usize  = 3;
+    let mut countdown : usize  = 3;
     let mut active_menu_item = MenuItem::Home;
+    let mut game_start_time: Option<Instant> = None;
+
+    let game_words = vec!["the", "lazy", "dog", "jumped", "over", "the", "quick", "brown", "fox"];
+
     loop {
         terminal.draw(|rect| {
             let size = rect.size();
@@ -102,41 +109,63 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 )
                 .split(size);
 
-            let menu = menu_titles
-                .iter()
-                .map(|t| {
-                    let (first, rest) = t.split_at(1);
-                    Spans::from(vec![
-                        Span::styled(
-                            first,
-                            Style::default()
-                                .fg(Color::Yellow)
-                                .add_modifier(Modifier::UNDERLINED),
-                        ),
-                        Span::styled(rest, Style::default().fg(Color::White)),
-                    ])
-                })
-                .collect();
-
-            let tabs = Tabs::new(menu)
-                .block(Block::default().title("Menu").borders(Borders::ALL))
-                .style(Style::default().fg(Color::White)) // color non-active items white 
-                .highlight_style(Style::default().fg(Color::Yellow)) // color active_menu_item yellow 
-                .divider(Span::raw("|"));
-
-            rect.render_widget(tabs, chunks[0]);
             match active_menu_item {
-                MenuItem::Home => rect.render_widget(render_home(start, &mut i), chunks[1]),
+                MenuItem::Home => {
+                let menu = menu_titles
+                    .iter()
+                    .map(|t| {
+                        let (first, rest) = t.split_at(1);
+                        Spans::from(vec![
+                            Span::styled(
+                                first,
+                                Style::default()
+                                    .fg(Color::Yellow)
+                                    .add_modifier(Modifier::UNDERLINED),
+                            ),
+                            Span::styled(rest, Style::default().fg(Color::White)),
+                        ])
+                    })
+                    .collect();
+    
+                let tabs = Tabs::new(menu)
+                    .block(Block::default().title("Menu").borders(Borders::ALL))
+                    .style(Style::default().fg(Color::White)) // color non-active items white 
+                    .highlight_style(Style::default().fg(Color::Yellow)) // color active_menu_item yellow 
+                    .divider(Span::raw("|"));
+    
+                    rect.render_widget(tabs, chunks[0]);
+                    rect.render_widget(render_home(start, &mut countdown), chunks[1]);
+                    if countdown == 0 {
+                        active_menu_item = MenuItem::Game;
+                        game_start_time = Some(Instant::now());
+                    }
+                },
                 MenuItem::Game => {
-                    let pw_chunks = Layout::default()
-                        .direction(Direction::Horizontal)
-                        .constraints(
-                            [Constraint::Percentage(20), Constraint::Percentage(80)].as_ref(),
-                        )
-                        .split(chunks[1]); // split the content area for pets menu into 20 80 ratio
-                    //let (left, right) = render_pets(&pet_list_state);
-                    //rect.render_stateful_widget(left, pets_chunks[0], &mut pet_list_state);
-                    // rect.render_widget(right, pw_chunks[1]);
+                    let text = Spans::from(vec![Span::raw(curr_input.as_str())]);
+                    let input = Paragraph::new(text).block(Block::default().title("Input").borders(Borders::ALL));
+
+                    rect.render_widget(input, chunks[0]);
+                    
+                    let in_game_timer = game_start_time.unwrap().elapsed().as_secs();
+
+                    let mut percent = 100 - ((100.0 * in_game_timer as f32) / 60.0) as i32;
+                    if percent < 0 {
+                        percent = 0;
+                    }
+
+                    let progress_bar = Gauge::default()
+                        .block(Block::default()
+                        .borders(Borders::ALL)
+                        .title("Time Remaining"))
+                        .gauge_style(Style::default().fg(Color::White).bg(Color::Black))
+                        .percent(percent as u16);
+                    
+                    rect.render_widget(progress_bar, chunks[2]);
+
+                    
+                },
+                MenuItem::GameOver => {
+
                 }
             };
         })?;
@@ -144,13 +173,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         if start {
             match rx.recv()? {
                 Event::Input(event) => match event.code {
-                    KeyCode::Char('q') => {
-                        disable_raw_mode()?;
-                        terminal.show_cursor()?;
-                        break;
-                    }
-                    KeyCode::Char('h') => active_menu_item = MenuItem::Home,
-                    KeyCode::Char('s') => start = true,
+                    KeyCode::Backspace => {
+                        curr_input.pop();
+                    },
+                    KeyCode::Char(c) => {
+                        curr_input.push(c);
+                    },
                     _ => {}
                 },
                 Event::Tick => {}
@@ -175,7 +203,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn render_home<'a>(start: bool, i: &mut usize) -> Paragraph<'a> {
+fn render_home<'a>(start: bool, countdown: &mut usize) -> Paragraph<'a> {
     let mut text = vec![
         Spans::from(vec![Span::raw("")]),
         Spans::from(vec![Span::raw("Welcome")]),
@@ -188,13 +216,13 @@ fn render_home<'a>(start: bool, i: &mut usize) -> Paragraph<'a> {
         )]),
         Spans::from(vec![Span::raw("")]),
     ];
-    if start && *i > 0 {
+    if start && *countdown > 0 {
         thread::sleep(Duration::from_secs(1));
         let mut s = String::from("Starting race in ");
-        s.push_str(i.to_string().as_str());
+        s.push_str(countdown.to_string().as_str());
         let span = Spans::from(vec![Span::raw(s.as_str().to_owned())]);
         text.push(span);
-        *i = *i - 1;
+        *countdown = *countdown - 1;
     }
     let home = Paragraph::new(text)
     .alignment(Alignment::Center)
